9eae0e535bd0664acc1ed12572e1006dc139e720
lorenaparreno <137244224+lorenaparreno@users.noreply.github.com>
2024-02-11 19:57:47 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+let challenge2Element = document.querySelector("#challenge-2");
+let text = "Lorena Parreno";
+let index = 0;
+
+function typeText() {
+  if (index < text.length) {
+    challenge2Element.innerHTML += text.charAt(index);
+    index++;
+    setTimeout(typeText, 100);
+  }
+}
+
+typeText();
+
+
+
+new Typewriter("h2", {
+  strings: "Lorena Parreno",
+  autoStart: true,
+  cursor: "",
+  delay: 10,
+});
+
